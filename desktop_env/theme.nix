be71f0263edd8b3b31e71bfd986533c31ecdd0d1
gusjengis <anthony.j.green@outlook.com>
2026-02-12 17:24:03 -0800

    restored theme, not notifying if dunstify is missing

--- desktop_env/theme.nix
+++ desktop_env/theme.nix
@@ -6,22 +6,20 @@
 }:
 
 {
-  config = lib.mkIf config.desktopEnv.enable {
-    home.sessionVariables = {
-      GTK_THEME = "Adwaita:dark";
-      QT_STYLE_OVERRIDE = "kvantum";
-      QT_QPA_PLATFORMTHEME = "qt5ct";
-    };
-
-    gtk = {
-      enable = true;
-      theme.name = "Adwaita-dark";
-      theme.package = pkgs.gnome-themes-extra;
-    };
-
-    xdg.configFile."Kvantum/kvantum.kvconfig".text = ''
-      [General]
-      theme=KvAdaptaDark
-    '';
+  home.sessionVariables = {
+    GTK_THEME = "Adwaita:dark";
+    QT_STYLE_OVERRIDE = "kvantum";
+    QT_QPA_PLATFORMTHEME = "qt5ct";
   };
+
+  gtk = {
+    enable = true;
+    theme.name = "Adwaita-dark";
+    theme.package = pkgs.gnome-themes-extra;
+  };
+
+  xdg.configFile."Kvantum/kvantum.kvconfig".text = ''
+    [General]
+    theme=KvAdaptaDark
+  '';
 }
--- scripts/update.sh
+++ scripts/update.sh
@@ -6,7 +6,13 @@ hm_repo="$HOME/.home-manager"
 modules_repo="/etc/nix-modules"
 sync_script="$hm_repo/scripts/sync-repos.sh"
 
-dunstify_bin="$HOME/.nix-profile/bin/dunstify"
+dunstify_bin="$(command -v dunstify || true)"
+
+notify() {
+  if [[ -n "$dunstify_bin" ]]; then
+    "$dunstify_bin" "$@" || true
+  fi
+}
 
 hm_before="$(git -C "$hm_repo" rev-parse HEAD)"
 modules_before="$(git -C "$modules_repo" rev-parse HEAD)"
@@ -18,16 +24,16 @@ modules_after="$(git -C "$modules_repo" rev-parse HEAD)"
 
 if [[ "$modules_before" != "$modules_after" ]]; then
   if ! sudo nixos-rebuild switch --impure --flake /etc/nixos/; then
-    "$dunstify_bin" --urgency=critical "nixos-rebuild" "nix-modules changed, rebuild failed"
+    notify --urgency=critical "nixos-rebuild" "nix-modules changed, rebuild failed"
   else
-    "$dunstify_bin" "nixos-rebuild" "rebuild succeeded" || true
+    notify "nixos-rebuild" "rebuild succeeded"
   fi
 fi
 
 if [[ "$hm_before" != "$hm_after" ]]; then
   if ! home-manager switch --impure --flake ~/.home-manager/; then
-    "$dunstify_bin" --urgency=critical "home-manager" "home-manager changed, switch failed"
+    notify --urgency=critical "home-manager" "home-manager changed, switch failed"
   else
-    "$dunstify_bin" "home-manager" "rehome succeeded" || true
+    notify "home-manager" "rehome succeeded"
   fi
 fi
