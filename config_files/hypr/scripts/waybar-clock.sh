f5c8594863ac73445adbf2fc569030bb0e7f763f
gusjengis <anthony.j.green@outlook.com>
2026-03-06 21:14:48 -0800

    more style tweaks

--- config_files/hypr/scripts/waybar-clock.sh
+++ config_files/hypr/scripts/waybar-clock.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+clock_text="$(date '+%a %d  %I:%M%P')"
+clock_text="${clock_text/%m/}"
+
 printf '{"text":"%s","tooltip":"%s"}\n' \
-  "$(date '+%a %b %d  %I:%M %P')" \
+  "$clock_text" \
   "$(date '+%Y-%m-%d %H:%M:%S')"
