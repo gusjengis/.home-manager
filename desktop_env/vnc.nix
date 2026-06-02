5e1ee436570a47bf78b286d6213bebe056892891
gusjengis <anthony.j.green@outlook.com>
2026-06-01 19:56:30 -0700

    more vnc stuff

--- desktop_env/vnc.nix
+++ desktop_env/vnc.nix
@@ -5,6 +5,24 @@
   ...
 }:
 
+let
+  vncviewerRemote = pkgs.writeShellScriptBin "vncviewer-remote" ''
+    set -euo pipefail
+
+    if [ "$#" -eq 0 ]; then
+      printf 'Usage: vncviewer-remote host[:display] | host::port\n' >&2
+      exit 64
+    fi
+
+    exec ${pkgs.tigervnc}/bin/vncviewer \
+      -FullScreen \
+      -FullscreenSystemKeys \
+      -AlwaysCursor \
+      -RemoteResize=0 \
+      -Shared \
+      "$@"
+  '';
+in
 {
   config = lib.mkIf config.desktopEnv.enable {
     services.wayvnc = {
@@ -14,12 +32,16 @@
         address = "0.0.0.0";
         port = 5900;
         enable_auth = false;
+        xkb_layout = "us";
       };
     };
+
+    systemd.user.services.wayvnc.Service.ExecStart = lib.mkForce "${config.home.homeDirectory}/.home-manager/scripts/start-wayvnc.sh";
 
     home.packages = with pkgs; [
       remmina
       tigervnc
+      vncviewerRemote
     ];
   };
 }
