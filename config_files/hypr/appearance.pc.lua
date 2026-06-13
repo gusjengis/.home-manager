21f8ce00cda2698dc312d8b787a68c1ba59e334f
gusjengis <anthony.j.green@outlook.com>
2026-06-12 20:10:14 -0700

    messing with appearance config to adapt to changes in window blur

--- config_files/hypr/appearance.pc.lua
+++ config_files/hypr/appearance.pc.lua
@@ -7,52 +7,51 @@ hl.env("XCURSOR_THEME", cursor_theme)
 hl.env("XCURSOR_SIZE", tostring(cursor_size))
 
 hl.config({
-    general = {
-        gaps_in = 5,
-        gaps_out = 0,
-        border_size = 2,
-        col = {
-            active_border = "rgba(595959ff)",
-            inactive_border = "rgba(595959aa)",
-        },
-        resize_on_border = false,
-        allow_tearing = false,
-        layout = "dwindle",
-    },
-    decoration = {
-        rounding = 10,
-        rounding_power = 2,
-        active_opacity = 1.0,
-        inactive_opacity = 1.0,
-        shadow = {
-            enabled = true,
-            range = 4,
-            render_power = 3,
-            color = "rgba(1a1a1aee)",
-        },
-        blur = {
-            enabled = true,
-            size = 3,
-            passes = 1,
-            special = false,
-            occupied_workspace = false,
-            vibrancy = 0.1696,
-        },
-        dim_special = 0.0,
-    },
-    animations = {
-        enabled = true,
-    },
-    dwindle = {
-        preserve_split = true,
-    },
-    master = {
-        new_status = "master",
-    },
-    misc = {
-        force_default_wallpaper = 0,
-        disable_hyprland_logo = false,
-    },
+	general = {
+		gaps_in = 5,
+		gaps_out = 0,
+		border_size = 2,
+		col = {
+			active_border = "rgba(595959ff)",
+			inactive_border = "rgba(595959aa)",
+		},
+		resize_on_border = false,
+		allow_tearing = false,
+		layout = "dwindle",
+	},
+	decoration = {
+		rounding = 10,
+		rounding_power = 2,
+		active_opacity = 1.0,
+		inactive_opacity = 1.0,
+		shadow = {
+			enabled = true,
+			range = 4,
+			render_power = 3,
+			color = "rgba(1a1a1aee)",
+		},
+		blur = {
+			enabled = true,
+			size = 3,
+			passes = 1,
+			special = false,
+			vibrancy = 0.1696,
+		},
+		dim_special = 0.0,
+	},
+	animations = {
+		enabled = true,
+	},
+	dwindle = {
+		preserve_split = true,
+	},
+	master = {
+		new_status = "master",
+	},
+	misc = {
+		force_default_wallpaper = 0,
+		disable_hyprland_logo = false,
+	},
 })
 
 hl.curve("easeOutQuint", { type = "bezier", points = { { 0.23, 1 }, { 0.32, 1 } } })
