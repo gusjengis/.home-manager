b6f0f0bdfd708cb03f4bf4797360ecf88b0f6f1c
gusjengis <anthony.j.green@outlook.com>
2026-06-16 19:28:02 -0700

    new installing chromium even on headless servers

--- programs/browser.nix
+++ programs/browser.nix
@@ -10,27 +10,27 @@ let
   chromiumPkg = (pkgs.chromium.override { enableWideVine = true; });
 in
 {
-  config = lib.mkIf config.desktopEnv.enable {
-    programs.chromium = {
-      enable = true;
-      package = chromiumPkg;
-      commandLineArgs = [
-        "--ozone-platform=wayland"
-        "--ignore-gpu-blocklist"
-        "--hide-crash-restore-bubble"
-        "--disable-features=WaylandWpColorManagerV1"
-        # "--enable-unsafe-webgpu"
-      ];
-      extensions = [
-        "iobmefdldoplhmonnnkchglfdeepnfhd" # Google Search Keyboard Shortcuts
-        "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
-        "jpkfgepcmmchgfbjblnodjhldacghenp" # Pie Adblock
-        "aeblfdkhhhdcdjpifhhbdiojplfjncoa" # 1Password
-        "eiimnmioipafcokbfikbljfdeojpcgbh" # BlockSite
-        "mgngbgbhliflggkamjnpdmegbkidiapm" # Remove YouTube Shorts
-        "lcpclaffcdiihapebmfgcmmplphbkjmd" # Block YouTube Feed
-        "khncfooichmfjbepaaaebmommgaepoid" # Unhook
-      ];
-    };
+  # config = lib.mkIf config.desktopEnv.enable {
+  programs.chromium = {
+    enable = true;
+    package = chromiumPkg;
+    commandLineArgs = [
+      "--ozone-platform=wayland"
+      "--ignore-gpu-blocklist"
+      "--hide-crash-restore-bubble"
+      "--disable-features=WaylandWpColorManagerV1"
+      # "--enable-unsafe-webgpu"
+    ];
+    extensions = [
+      "iobmefdldoplhmonnnkchglfdeepnfhd" # Google Search Keyboard Shortcuts
+      "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
+      "jpkfgepcmmchgfbjblnodjhldacghenp" # Pie Adblock
+      "aeblfdkhhhdcdjpifhhbdiojplfjncoa" # 1Password
+      "eiimnmioipafcokbfikbljfdeojpcgbh" # BlockSite
+      "mgngbgbhliflggkamjnpdmegbkidiapm" # Remove YouTube Shorts
+      "lcpclaffcdiihapebmfgcmmplphbkjmd" # Block YouTube Feed
+      "khncfooichmfjbepaaaebmommgaepoid" # Unhook
+    ];
   };
+  # };
 }
